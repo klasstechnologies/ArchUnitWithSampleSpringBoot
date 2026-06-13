a6378f81bad69de7d163b60deffcad3603de1121
kaplimonadi <kapilmonadi@gmail.com>
2026-06-13 20:19:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringBootFeaturesSample
+A sample project showcasing the capabilities being provided by the different modules in Spring Boot
---
+++ src/test/java/com/klasstechnologies/ArchUnitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klasstechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchUnitSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
